a7631f961b17712d7d6056e88ccfb3dae0c214bb
Clifton <clifga@gmail.com>
2023-03-26 15:19:09 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Add your code here
+function submitData(name,email){
+
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({"name":name, "email":email})
+        })
+        .then(response => response.json())
+        .then(data => {document.body.textContent = data.id})
+        .catch(error => {document.body.textContent = error.message})
+    }
